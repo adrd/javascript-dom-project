a1b32db7b52691c8a20cddadb4d4c4e0f97bd54a
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-02 11:25:41 +0300

    Lesson 3 - Understanding DOM Loading Events.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import '../assets/css/style.css';
+import "../assets/css/style.css";
 
 const app = document.getElementById('app');
 
 app.innerHTML = `<h1>JavaScript DOM</h1>`;
+
+document.addEventListener("DOMContentLoaded", () => {
+    // const app = document.getElementById("app");
+
+    // app.innerHTML = `<h1>JavaScript DOM</h1>`;
+
+    alert("DOMContentLoaded");
+});
+
+window.addEventListener("load", () => alert("Load"));
