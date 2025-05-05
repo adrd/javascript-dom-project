79f21b829a204975199c4192b0ff22aa2baecde5
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-05 14:24:03 +0300

    Lesson 7 - Changing Content of DOM Elements.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import "../assets/css/style.css";
 
 const app = document.getElementById('app');
+// app.innerText = '<span>Some Text Here</span>'; 
+app.innerHTML = `
+  <h1>JavaScript DOM</h1>
+`;                                  // innertHTML receives a HTML template and creates the nodes of DOM in memory
+                                    // innerHTML overwrites innerText, if innerText was after innerHTML, innertText overwrites innerHTML 
 
-const div = document.createElement('div');
-const text = document.createTextNode('DOM!');
-const comment = document.createComment('No comment');
+const h1 = document.createElement('h1');
+h1.innerText = 'Ultimate Courses';
+h1.innerText += 'Learning JS DOM';
+// h1.style.display = 'none';
+app.append(h1);
 
-div.append(comment);
-div.append(text);
-app.append(div);
-
-console.log(app, div);
+console.log(app.innerHTML);        // displays HTML template as string
+console.log(app.innerText);        // displays text only without whitespaces, displays visible text
+                                   // used only on Elements
+console.log(app.innerText.length);
+console.log(app.textContent);      // displays text with whitespaces, displays full text being visible or not
+                                   // applies to all Nodes
