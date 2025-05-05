dab7fd22127021c942ef871056894eaaa82fec84
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-05 13:28:05 +0300

    Lesson 6 - Creating DOM Nodes.

--- src/index.js
+++ src/index.js
@@ -2,55 +2,12 @@ import "../assets/css/style.css";
 
 const app = document.getElementById('app');
 
-app.innerHTML = `<h1>JavaScript DOM</h1>`;
+const div = document.createElement('div');
+const text = document.createTextNode('DOM!');
+const comment = document.createComment('No comment');
 
-// <DOCTYPE>
-console.log(document);
-console.dir(document);
+div.append(comment);
+div.append(text);
+app.append(div);
 
-// <html>
-console.log(document.documentElement);
-console.dir(document.documentElement);
-
-// <head>
-console.dir(document.head);
-
-// <body>
-console.dir(document.body);
-
-// retrieve the constructor name
-console.log(document.body.constructor.name);            // HTMLBodyElement
-
-// looking at the prototype chain
-console.log(document.body instanceof HTMLBodyElement);  // true
-console.log(document.body instanceof HTMLElement);      // true
-console.log(document.body instanceof Element);          // true
-console.log(document.body instanceof Node);             // true
-console.log(document.body instanceof EventTarget);      // true
-
-/* 
-  - NodeTypes
-  1. Element
-  2. Attribute
-  3. Text
-  4. CDATASection
-  5. EntityReference
-  6. Entity
-  7. ProcessingInstruction
-  8. Comment
-  9. Document
-  10. DocumentType
-  11. DocumentFragment
-  12. Notation
-*/
-
-console.log('body node is of type = ' + document.body.nodeType); // 1 = Element
-console.log('document node is of type = ' + document.nodeType);  // 9 = Document
-
-// use nodeName for any Node types 
-console.log('body node name is = '+ document.body.nodeName);     // BODY
-// use tagName for any Element types
-console.log('body tag name is = ' + document.body.tagName);      // BODY
-
-console.log('document node name is = ' + document.nodeName);     // #document
-console.log('document tag name is = ' + document.tagName);       // undefined
+console.log(app, div);
