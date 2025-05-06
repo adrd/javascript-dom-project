2daf804fe605a467d33d12974c2a08406c81380e
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-06 11:57:28 +0300

    Lesson 11 - Inserting DOM as String Templates.

--- src/index.js
+++ src/index.js
@@ -4,41 +4,48 @@ const app = document.getElementById('app');
 
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
+  <ul>
+    <li>1</li>
+  </ul>
 `;
 
-const div = document.createElement('div');
-const span = document.createElement('span');
-const p = document.createElement('p');
-const i = document.createElement('i');
-const b = document.createElement('b');
+const ul = document.querySelector('ul');
 
-// new way using new api
-div.append(span);
-div.prepend(p);
-// span.before(i);
-// span.after(i);
-p.after(i);         // <div>
-                    //  <p></p>
-                    //  <i></i>
-                    //  <span></span>
-                    // </div>
+// ul.insertAdjacentHTML('beforebegin', '<li>X</li>');
+ul.insertAdjacentHTML('beforebegin', '<p>Before</p>');  // <div id="app">
+                                                        //  <h1>JavaScript DOM</h1>
+                                                        //  <p>Before</p>
+                                                        //  <ul>
+                                                        //    <li>1</li>
+                                                        //  </ul>
+                                                        // </div>
 
-// console.log(div);
+ul.insertAdjacentHTML('afterbegin', '<li>First</li>');  // <div id="app">
+                                                        //  <h1>JavaScript DOM</h1>
+                                                        //  <p>Before</p>
+                                                        //  <ul>
+                                                        //    <li>First</li>
+                                                        //    <li>1</li>
+                                                        //  </ul>
+                                                        // </div>
 
-// Before: old way using insertBefore
-i.parentNode.insertBefore(b, i);   // <div>
-                                   //   <p></p>
-                                   //   <b></b>
-                                   //   <i></i>
-                                   //   <span></span>
-                                   // </div> 
+ul.insertAdjacentHTML('beforeend', '<li>Last</li>')     // <div id="app">
+                                                        //  <h1>JavaScript DOM</h1>
+                                                        //  <p>Before</p>
+                                                        //  <ul>
+                                                        //    <li>First</li>
+                                                        //    <li>1</li>
+                                                        //    <li>Last</li>
+                                                        //  </ul>
+                                                        // </div>
 
-// After: old way using insertBefore + nextSibling
-i.parentNode.insertBefore(b, i.nextSibling);   // <div>
-                                               //   <p></p>
-                                               //   <i></i>
-                                               //   <b></b>
-                                               //   <span></span>
-                                               // </div>
-
-console.log(div);
+ul.insertAdjacentHTML('afterend', '<p>After</p>');      // <div id="app">
+                                                        //  <h1>JavaScript DOM</h1>
+                                                        //  <p>Before</p>
+                                                        //  <ul>
+                                                        //    <li>First</li>
+                                                        //    <li>1</li>
+                                                        //    <li>Last</li>
+                                                        //  </ul>
+                                                        //  <p>After</p>
+                                                        // </div>
