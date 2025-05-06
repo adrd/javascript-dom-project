0fd09d79cfa82c253ad4a4b65ef23f6bf765cbcf
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-06 19:10:18 +0300

    Lesson 19 - Finding Parent Elements.

--- src/index.js
+++ src/index.js
@@ -4,43 +4,27 @@ const app = document.getElementById('app');
 
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
-  <ul id="list">
-    <li>Earth</li>
-    <li>Fire</li>
-    <li>Water</li>
-    <li>Air</li>
-  </ul>
+  <div class="item"></div>
 `;
 
 // querySelectorAll: NodeList
-const list = document.querySelector('#list');
-const queryChildren = list.querySelectorAll('li');
-const selectedIndex = 2;
+const item = document.querySelector('.item');
 
-console.log(queryChildren);                 // NodeList(4) [li, li, li, li]
-console.log(queryChildren.length);          // 4
-console.log(queryChildren[selectedIndex]);  // <li>Water</li>
+console.log(item.parentNode);                   // <div id="app">
+                                                //  <h1>JavaScript DOM</h1>
+                                                //  <div class="item"></div>
+                                                // </div>
 
-// .children: HTMLCollection
-console.log(list.children);                 // HTMLCollection(4) [li, li, li, li]
-console.log(list.children.length);          // 4
-console.log(list.children[selectedIndex]);  // <li>Water</li>
+console.log(item.parentElement);                // <div id="app">
+                                                //  <h1>JavaScript DOM</h1>
+                                                //  <div class="item"></div>
+                                                // </div>
 
-// .childNodes: NodeList
-console.log(list.childNodes);                // NodeList(9) [text, li, text, li, text, li, text, li, text]
-console.log(list.childNodes.length);         // 9
-console.log(list.childNodes[selectedIndex]); // #text
+console.log(item.parentElement.parentElement);  // <body>...</body>
 
-console.log(document.children);              // HTMLCollection [html]
+console.log(item.closest('#app'));              // <div id="app">
+                                                //  <h1>JavaScript DOM</h1>
+                                                //  <div class="item"></div>
+                                                // </div>
 
-console.log(document.body.children);         // HTMLCollection(3) [header.header, div#app, script, app: div#app]
-
-console.log(document.body.childNodes);       // NodeList(7) [text, header.header, text, div#app, text, script, text]
-
-// first
-console.log(list.firstChild);                // #text
-console.log(list.firstElementChild);         // <li>Earth</li>
-
-// last
-console.log(list.lastChild);                 // #text
-console.log(list.lastElementChild);          // <li>Air</li>
+console.log(item.closest('body'));              // <body>...</body>
