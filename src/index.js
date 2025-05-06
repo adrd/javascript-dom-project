64993445b89772ec411bc896e290e2d524d58627
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-06 14:15:19 +0300

    Lesson 15 - Querying DOM Nodes (HTMLCollections).

--- src/index.js
+++ src/index.js
@@ -4,22 +4,45 @@ const app = document.getElementById('app');
 
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
+  <ul id="list"></ul>
 `;
 
-const div = document.createElement('div');
-div.innerText = 'I am a message!'; 
+const data = ['Earth', 'Fire', 'Water'];
+const fragment = document.createDocumentFragment();
 
-app.append(div);  // <div id="app">
-                  //  <h1>JavaScript DOM</h1>
-                  //  <div>I am a message!</div>
-                  // </div>
+data.forEach(item => {
+  const li = document.createElement('li');
+  li.className = 'list-item';
+  li.innerText = item;
+  fragment.append(li);
+});
 
-// new way
-setTimeout(() => div.remove(), 2500);  // <div id="app">
-                                       //   <h1>JavaScript DOM</h1>
-                                       // </div>
+// getElementById: HTMLElement
+const ulFromId = document.getElementById('list');
+console.log(ulFromId);
+ulFromId.append(fragment);   // <div id="app">
+                             //   <h1>JavaScript DOM</h1>
+                             //   <ul id="list">
+                             //     <li class="list-item">Earth</li>
+                             //     <li class="list-item">Fire</li>
+                             //     <li class="list-item">Water</li>
+                             //   </ul>
+                             // </div>
 
-// old way
-// setTimeout(() => div.parentNode.removeChild(div), 2500);  // <div id="app">
-                                                          //   <h1>JavaScript DOM</h1>
-                                                          // </div>
+// getElementsByClassName: HTMLCollection
+const listItemsFromClassName = ulFromId.getElementsByClassName('list-item');
+console.log(listItemsFromClassName);  // HTMLCollection(3) [li.list-item, li.list-item, li.list-item]
+
+// getElementsByTagName: HTMLCollection
+const listItemsFromTagName = ulFromId.getElementsByTagName('li');
+console.log(listItemsFromTagName);    // HTMLCollection(3) [li.list-item, li.list-item, li.list-item]
+
+// Demonstrate live collection
+const newListItem = document.createElement('li');
+newListItem.className = 'list-item';
+newListItem.innerText = 'Air';
+ulFromId.append(newListItem);
+
+// No need to query again!
+console.log(listItemsFromClassName);  // HTMLCollection(4) [li.list-item, li.list-item, li.list-item, li.list-item]
+console.log(listItemsFromTagName);    // HTMLCollection(4) [li.list-item, li.list-item, li.list-item, li.list-item]
