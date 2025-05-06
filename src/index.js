9d86153d41e0004503b1f2aed5aa32a64f51840a
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-06 15:40:59 +0300

    Lesson 17 - Looping over DOM Elements.

--- src/index.js
+++ src/index.js
@@ -4,45 +4,35 @@ const app = document.getElementById('app');
 
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
-  <ul id="list"></ul>
+  <ul id="list">
+    <li>Earth</li>
+    <li>Fire</li>
+    <li>Water</li>
+    <li>Air</li>
+  </ul>
 `;
 
-const data = ['Earth', 'Fire', 'Water'];
-const fragment = document.createDocumentFragment();
+const listItems = document.querySelectorAll('#list li');
+console.log(listItems);  // NodeList(4) [li, li, li, li]
 
-data.forEach(item => {
-  const li = document.createElement('li');
-  li.className = 'list-item';
-  li.innerText = item;
-  fragment.append(li);
-});
+for (let index = 0; index < listItems.length; index++) {
+  console.log(listItems[index]);  // <li>Earth</li>
+}                                 // <li>Fire</li>
+                                  // <li>Water</li>
+                                  // <li>Air</li>
 
-// ul { background: red; }
-// #list { background: red; }
-const ulFromQuerySelector = document.querySelector('ul');
-console.log(ulFromQuerySelector);   // <ul id="list"></ul>
+for (const item of listItems) {
+  console.log(item);              // <li>Earth</li>
+}                                 // <li>Fire</li>
+                                  // <li>Water</li>
+                                  // <li>Air</li>
 
-const ulFromQuerySelector1 = document.querySelector('#list');
-console.log(ulFromQuerySelector1);  // <ul id="list"></ul>
+[...listItems].forEach((item) => console.log(item));         // <li>Earth</li>
+                                                             // <li>Fire</li>
+                                                             // <li>Water</li>
+                                                             // <li>Air</li>
 
-ulFromQuerySelector.append(fragment);  // <div id="app">
-                                       //  <h1>JavaScript DOM</h1>
-                                       //  <ul id="list">
-                                       //   <li class="list-item">Earth</li>
-                                       //   <li class="list-item">Fire</li>
-                                       //   <li class="list-item">Water</li>
-                                       //  </ul>
-                                       // </div>
-
-const listItemsFromQSA = ulFromQuerySelector.querySelectorAll('.list-item');
-console.log(listItemsFromQSA);      // NodeList(3) [li.list-item, li.list-item, li.list-item]
-
-const newListItem = document.createElement('li');
-newListItem.className = 'list-item';
-newListItem.innerText = 'Air';
-ulFromQuerySelector.append(newListItem);
-
-console.log(listItemsFromQSA);      // NodeList(3) [li.list-item, li.list-item, li.list-item]
-
-// need to query again!
-console.log(ulFromQuerySelector.querySelectorAll('.list-item'));  // NodeList(4) [li.list-item, li.list-item, li.list-item, li.list-item]
+Array.from(listItems).forEach((item) => console.log(item));  // <li>Earth</li>
+                                                             // <li>Fire</li>
+                                                             // <li>Water</li>
+                                                             // <li>Air</li>
