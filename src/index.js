1c6e31ff9fed97244c4a3f5f042854ab18fc8fe5
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-06 13:11:08 +0300

    Lesson 14 - Removing DOM Elements.

--- src/index.js
+++ src/index.js
@@ -7,34 +7,19 @@ app.innerHTML = `
 `;
 
 const div = document.createElement('div');
-const span = document.createElement('span');
+div.innerText = 'I am a message!'; 
 
-span.innerText = 'Can you clone me?';
-div.append(span);
+app.append(div);  // <div id="app">
+                  //  <h1>JavaScript DOM</h1>
+                  //  <div>I am a message!</div>
+                  // </div>
 
-app.append(div);        // <div id="app">
-                        //  <h1>JavaScript DOM</h1>
-                        //  <div>
-                        //    <span>Can you clone me?</span>
-                        //  </div>
-                        // </div>
+// new way
+setTimeout(() => div.remove(), 2500);  // <div id="app">
+                                       //   <h1>JavaScript DOM</h1>
+                                       // </div>
 
-// cloneNode(false) only clones the top element
-const clone = div.cloneNode();
-console.log(clone);     // <div></div>
-
-// cloneNode(true) clones all elements and subtrees
-const newClone = div.cloneNode(true);
-console.log(newClone);  // <div>
-                        //  <span>Can you clone me?</span>
-                        // </div>
-
-app.append(newClone);   // <div id="app">
-                        //  <h1>JavaScript DOM</h1>
-                        //  <div>
-                        //    <span>Can you clone me?</span>
-                        //  </div>
-                        //  <div>
-                        //    <span>Can you clone me?</span>
-                        //  </div>
-                        // </div>
+// old way
+// setTimeout(() => div.parentNode.removeChild(div), 2500);  // <div id="app">
+                                                          //   <h1>JavaScript DOM</h1>
+                                                          // </div>
