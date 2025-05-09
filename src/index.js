44b729a6615010412141d64bf94d21bde23378e8
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-09 14:25:16 +0300

    Lesson 27 - Event Bubbling, Capturing and Propagation.

--- src/index.js
+++ src/index.js
@@ -4,40 +4,41 @@ const app = document.getElementById('app');
 
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
-  <button type="button">
-    Click Me
-  </button>
+  <div class="one">
+    <div class="two">
+      <button type="button" class="three">
+        Click Me
+      </button>
+    </div>
+  </div>
 `;
 
-const button = document.querySelector('button');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
 
-// function handleClick(event) {
-//   console.log(event.target);        // element that initiated the event, in this case button element
-// }
-
-// button.addEventListener('click', handleClick);
-
-// button.removeEventListener('click', handleClick);
-
-// setTimeout(() => {
-//   button.removeEventListener('click', handleClick);
-// }, 5000);  // after 5 seconds click event handler is not available anymore
-
-/****************************************/
-
-function handleClickOnce(event) {
-  console.log(event.target);
-  button.removeEventListener('click', handleClickOnce);
+function handleClick(event) {
+  event.stopPropagation();       // event is not handled by div with class one and div with class two, only by the two button handlers
+  // event.stopImmediatePropagation(); // event is not handled by the other button handler and by div.one and div.two
+  console.log(event.target);        // <button type="button" class="three">Click Me</button>
 }
 
-button.addEventListener('click', handleClickOnce);
+// one.addEventListener('click', handleClick);
+// two.addEventListener('click', handleClick);
+// three.addEventListener('click', handleClick); 
 
-/****************************************/
+// after button click the event is bubbling and is handled by all three handlers
 
-button.addEventListener(
-  'dblclick', 
-  () => console.log('Double-click!'),
-  { once: true } 
-);
+one.addEventListener('click', handleClick, false);  // false means we don't want to use event capturing
+two.addEventListener('click', handleClick, false);
+three.addEventListener('click', handleClick, false); 
 
-// dblclick event handler is executed only once, after that it is removed
+three.addEventListener('click', (event) => console.log(event)); // > PointerEvent
+
+// three.addEventListener('click', (event) => console.log(event), { capture : true});
+
+// capturing phase = from the top of the DOM tree all the way down the event is initiated
+// target phase = when it reaches the element
+// bubbling phase = when it reached the element the bubbling phase begin, 
+//                  the event now is propagating back to the top of the DOM tree unless event.stopPropagation() 
+//                  is called in a handler
