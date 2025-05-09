2fe9804ecc4f97a67d56641b96bc8aabe01be3c4
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-09 18:31:01 +0300

    Lesson 28 - Preventing Default Event Actions.

--- src/index.js
+++ src/index.js
@@ -4,41 +4,37 @@ const app = document.getElementById('app');
 
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
-  <div class="one">
-    <div class="two">
-      <button type="button" class="three">
-        Click Me
-      </button>
-    </div>
-  </div>
+  <form>
+    <label>
+      Sign-up Email
+      <input type="email">
+    </label>
+    <label>
+      I agree to the terms
+      <input type="checkbox">
+    </label>
+  </form>
 `;
 
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
+const form = document.querySelector('form');
+const email = form.querySelector('input[type="email"]');
+const checkbox = form.querySelector('input[type="checkbox"]');
 
-function handleClick(event) {
-  event.stopPropagation();       // event is not handled by div with class one and div with class two, only by the two button handlers
-  // event.stopImmediatePropagation(); // event is not handled by the other button handler and by div.one and div.two
-  console.log(event.target);        // <button type="button" class="three">Click Me</button>
+function handleSubmit(event) {
+  // console.log(event);     // SubmitEvent 
+  // event.preventDefault();
+  if (!checkbox.checked) {
+    event.preventDefault();
+    console.log('I am not submitting...');
+    console.log(event.defaultPrevented);  // true
+    return;
+  }
+  
+  console.log('Submitted', email.value);
 }
 
-// one.addEventListener('click', handleClick);
-// two.addEventListener('click', handleClick);
-// three.addEventListener('click', handleClick); 
+form.addEventListener('submit', handleSubmit);
 
-// after button click the event is bubbling and is handled by all three handlers
-
-one.addEventListener('click', handleClick, false);  // false means we don't want to use event capturing
-two.addEventListener('click', handleClick, false);
-three.addEventListener('click', handleClick, false); 
-
-three.addEventListener('click', (event) => console.log(event)); // > PointerEvent
-
-// three.addEventListener('click', (event) => console.log(event), { capture : true});
-
-// capturing phase = from the top of the DOM tree all the way down the event is initiated
-// target phase = when it reaches the element
-// bubbling phase = when it reached the element the bubbling phase begin, 
-//                  the event now is propagating back to the top of the DOM tree unless event.stopPropagation() 
-//                  is called in a handler
+// checkbox.addEventListener('click', 
+//   event => event.preventDefault()
+// );
