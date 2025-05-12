63991c3ba1d9b924ce8d50f7afcbf820fb1631e9
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-12 11:38:45 +0300

    Lesson 31 - Accessing Forms and Elements.

--- src/index.js
+++ src/index.js
@@ -2,26 +2,57 @@ import "../assets/css/style.css";
 
 const app = document.getElementById('app');
 
+// app.innerHTML = `
+//   <h1>JavaScript DOM</h1>
+//   <form name="order">
+//     <label>
+//       Your name
+//       <input type="text" name="fullname">
+//     </label>
+//   </form>
+//   <form name="checkout">
+//     <label>
+//       Your name
+//       <input type="text" name="fullname">
+//     </label>
+//   </form>
+// `;
+
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
-  <div style="height: 1000px;"></div>
+  <form name="order">
+    <label>
+      Your name
+      <input type="text" name="fullname">
+    </label>
+  </form>
 `;
 
-document.addEventListener('keydown', event => {
-  // console.log(event.key, event.code);
-  switch (event.key) {
-    case 'ArrowUp': {
-      console.log('Up!');
-      event.preventDefault();
-      break;
-    }
-    case 'ArrowDown': {
-      console.log('Down!');
-      event.preventDefault();
-      break;
-    }
-  }
-});
+// const form = document.forms;
+// console.log(form);  // HTMLCollection [form, order: form]
 
-document.addEventListener('keydown', event => console.log(event.key));
-document.addEventListener('keyup', event => console.log(event.key));
+const form = document.forms.order;
+console.log(form);     // <form name="order">...</form>
+
+// const fullname = form.elements;
+// console.log(fullname);  // HTMLFormControlsCollection [input, fullname: input]
+
+const fullname = form.elements.fullname;
+console.log(fullname);     // <input type="text" name="fullname">
+
+// const { fullname, email } = form.elements;
+// console.log(fullname, email);  // <form name="order">...</form>   <input type="text" name="fullname">
+
+function handleInput(event) {
+  // console.log(event.target);   //  <input type="text" name="fullname">
+  
+  // access the value
+  console.log(event.target.value);
+
+  // access the parent form
+  console.log(event.target.form);
+}
+
+fullname.addEventListener('input', handleInput);
+
+// fullname.addEventListener('change', handleInput);  // fires when I leave the input element
