a9846cc6a66597c8f5f29bbc21e95e544864d4be
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-12 14:52:30 +0300

    Lesson 32 - Form Submit Event and FormData.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,6 @@ import "../assets/css/style.css";
 
 const app = document.getElementById('app');
 
-// app.innerHTML = `
-//   <h1>JavaScript DOM</h1>
-//   <form name="order">
-//     <label>
-//       Your name
-//       <input type="text" name="fullname">
-//     </label>
-//   </form>
-//   <form name="checkout">
-//     <label>
-//       Your name
-//       <input type="text" name="fullname">
-//     </label>
-//   </form>
-// `;
-
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
   <form name="order">
@@ -25,34 +9,46 @@ app.innerHTML = `
       Your name
       <input type="text" name="fullname">
     </label>
+    <label>
+      Which pizza would you like?
+      <select name="pizza">  
+        <option value="pepperoni">Pepperoni</option>
+        <option value="meaty">Meaty</option>
+        <option value="cheesey">Cheesey</option>
+      </select>
+    </label>
+    <button type="submit">
+      Submit
+    </button>
   </form>
 `;
 
-// const form = document.forms;
-// console.log(form);  // HTMLCollection [form, order: form]
-
 const form = document.forms.order;
-console.log(form);     // <form name="order">...</form>
 
-// const fullname = form.elements;
-// console.log(fullname);  // HTMLFormControlsCollection [input, fullname: input]
-
-const fullname = form.elements.fullname;
-console.log(fullname);     // <input type="text" name="fullname">
-
-// const { fullname, email } = form.elements;
-// console.log(fullname, email);  // <form name="order">...</form>   <input type="text" name="fullname">
-
-function handleInput(event) {
-  // console.log(event.target);   //  <input type="text" name="fullname">
-  
-  // access the value
-  console.log(event.target.value);
-
-  // access the parent form
-  console.log(event.target.form);
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(event);                      // SubmitEvent
+  console.log(event.target);               // <form name="order">...</form>
+  console.log(new FormData(event.target)); // event.target is a reference to the object onto which the event was dispatched
+                                           // new FormData(event.target) will trigger formData event
+  // console.log([...new FormData(event.target)]);  // (2) [Array(2), Array(2)]
 }
 
-fullname.addEventListener('input', handleInput);
+// [
+//   ["fullname", "Todd Motto"],
+//   ["email", ""]
+// ]
 
-// fullname.addEventListener('change', handleInput);  // fires when I leave the input element
+function handleFormData(event) {
+  // console.log(event.formData);             // > FormData {}
+  console.log([...event.formData]);           // > (2) [Array(2), Array(2)]
+  console.log([...event.formData.values()]);  // > ['Todd Motto', 'meaty']
+  // console.log([...event.formData.entries()]); // > (2) [Array(2), Array (2)]
+  const entries = event.formData.entries();
+  for (const entry of entries) {
+    console.log(entry);                       // (2) ['fullname', 'Todd Motto'], (2) ['pizza', 'meaty']
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('formdata', handleFormData);
