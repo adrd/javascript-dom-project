d4bdb8a812367bd211b8bcf65dadad615f1956a6
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-13 15:11:50 +0300

    Lesson 40 - Structuring our HTML Template.

--- src/index.js
+++ src/index.js
@@ -3,49 +3,22 @@ import "../assets/css/style.css";
 const app = document.getElementById('app');
 
 app.innerHTML = `
-  <h1>JavaScript DOM</h1>
-  <form name="example">
-    <select name="drink">
-      <option value="">Select Your Drink...</option>
-      <option value="lemonade" selected>Lemonade</option>
-      <option value="cola">Cola</option>
-      <option value="water">Water</option>
-    </select>
-  </form>
+  <div class="todos">
+    <div class="todos-header">
+      <h3 class="todos-title">Todo List</h3>
+      <div>
+        <p>You have <span class="todos-count"></span> items</p>
+        <button type="button" class="todos-clear" style="display: none;">
+          Clear Completed
+        </button>
+      </div>
+    </div>
+    <form class="todos-form" name="todos">
+      <input type="text" placeholder="What's next?" name="todo">
+    </form>
+    <ul class="todos-list">
+      
+    </ul>
+  </div>
 `;
 
-const form = document.forms.example;
-const select = form.elements.drink;
-
-// 1. Selected value
-select.value = 'water';
-console.log(select.value);
-
-// 2. Selected index
-console.dir(select);          // > select
-console.dir(select.options);  // > HTMLOptionsCollection(4)
-// console.dir(select.options.selectedIndex);
-
-const id = 2;
-select.selectedIndex = id;
-console.log(select.selectedIndex);
-
-// 3. Selected DOM Element
-// console.log(select.options);
-// console.log(select.options[0]);
-console.log(select.options[select.selectedIndex]);
-
-// 4. Events
-select.addEventListener('change', () => {
-  console.log(select.value);
-  console.log(select.selectedIndex);
-  console.log(select.options[select.selectedIndex]);
-});
-
-// 5. Add new <option>
-const option = document.createElement('option');
-option.value = 'milk';
-option.text = 'Milk';
-
-// select.append(option);
-select.add(option, 1);
