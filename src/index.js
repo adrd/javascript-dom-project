4eef9350845f07a26e7c20d121ac25e7431737ef
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-13 13:43:50 +0300

    Lesson 38 - Handling Select Elements.

--- src/index.js
+++ src/index.js
@@ -5,30 +5,47 @@ const app = document.getElementById('app');
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
   <form name="example">
-    <label>
-      Accept Marketing
-      <input type="checkbox" name="marketing" value="yes">
-    </label>
+    <select name="drink">
+      <option value="">Select Your Drink...</option>
+      <option value="lemonade" selected>Lemonade</option>
+      <option value="cola">Cola</option>
+      <option value="water">Water</option>
+    </select>
   </form>
 `;
 
 const form = document.forms.example;
-const checkbox = form.elements.marketing;
+const select = form.elements.drink;
 
-// 1. Properties that are useful
-console.dir(checkbox);  // > input
+// 1. Selected value
+select.value = 'water';
+console.log(select.value);
 
-// set
-checkbox.checked = true;
+// 2. Selected index
+console.dir(select);          // > select
+console.dir(select.options);  // > HTMLOptionsCollection(4)
+// console.dir(select.options.selectedIndex);
 
-// get
-console.log(checkbox.checked);  // true
+const id = 2;
+select.selectedIndex = id;
+console.log(select.selectedIndex);
 
-// 2. Events
-checkbox.addEventListener('change', () => {
-  console.log(checkbox.checked);
-  console.log(checkbox.value);
+// 3. Selected DOM Element
+// console.log(select.options);
+// console.log(select.options[0]);
+console.log(select.options[select.selectedIndex]);
+
+// 4. Events
+select.addEventListener('change', () => {
+  console.log(select.value);
+  console.log(select.selectedIndex);
+  console.log(select.options[select.selectedIndex]);
 });
 
-// 3. Methods
-checkbox.select();
+// 5. Add new <option>
+const option = document.createElement('option');
+option.value = 'milk';
+option.text = 'Milk';
+
+// select.append(option);
+select.add(option, 1);
