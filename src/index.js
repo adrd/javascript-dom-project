4ce40960ae5f6f575818d62af7f0fcef26d0d1aa
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-13 12:13:51 +0300

    Lesson 37 - Handling Checkbox Input Elements.

--- src/index.js
+++ src/index.js
@@ -5,59 +5,30 @@ const app = document.getElementById('app');
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
   <form name="example">
-    <div class="container">
-      <label>
-        Blue
-        <input type="radio" name="color" value="blue" checked>
-      </label>
-      <label>
-        Red
-        <input type="radio" name="color" value="red">
-      </label>
-      <label>
-        Green
-        <input type="radio" name="color" value="green">
-      </label>
-    </div>
+    <label>
+      Accept Marketing
+      <input type="checkbox" name="marketing" value="yes">
+    </label>
   </form>
 `;
 
 const form = document.forms.example;
-const radios = [...form.elements.color];
-
-console.log(radios);  // > (3) [input, input, input]
+const checkbox = form.elements.marketing;
 
 // 1. Properties that are useful
-// radios[2].checked = true;
+console.dir(checkbox);  // > input
 
-radios.forEach(radio => {
-  console.log(radio);          // <input type="radio" name="color" value="blue"> 
-                               // <input type="radio" name="color" value="red">
-                               // <input type="radio" name="color" value="green">
-  console.dir(radio);          // > input
-  console.log(radio.value);    // blue red green
-  console.log(radio.checked);  // true false false
-});
+// set
+checkbox.checked = true;
+
+// get
+console.log(checkbox.checked);  // true
 
 // 2. Events
-const container = form.querySelector('.container');
-// console.log(container);  // <div class="container">...</div>
-
-container.addEventListener('change', () => {
-  // console.log('Change...');
-  
-  // const checked = radios.find(radio => radio.checked).value;
-  // console.log(checked);
-
-  console.log(form.elements.color);        // > RadioNodeList(3) [input, input, input, value: 'green']
-  console.log(form.elements.color.value);
+checkbox.addEventListener('change', () => {
+  console.log(checkbox.checked);
+  console.log(checkbox.value);
 });
 
-// const checked = radios.find(radio => radio.checked);
-// console.log(checked);       // <input type="radio" name="color" value="blue" checked="">
-
-// const checkedValue = checked.value;
-// console.log(checkedValue);  // blue
-
 // 3. Methods
-radios[2].select();
+checkbox.select();
