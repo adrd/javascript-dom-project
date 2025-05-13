3593658b66223c8bef7af30d60a3bacabc73ff23
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-13 15:39:01 +0300

    Lesson 41 - State Initialization and Submit Events.

--- src/index.js
+++ src/index.js
@@ -17,8 +17,40 @@ app.innerHTML = `
       <input type="text" placeholder="What's next?" name="todo">
     </form>
     <ul class="todos-list">
-      
+
     </ul>
   </div>
 `;
 
+// state
+let todos = [];
+
+// selectors
+const form = document.forms.todos;
+const input = form.elements.todo;
+
+// functions
+function addTodo(event) {
+  event.preventDefault();
+  
+  const label = input.value.trim();
+  const complete = false;
+
+  todos = [
+    ...todos,
+    {
+      label,
+      complete
+    }
+  ];
+  console.log(todos);
+  input.value = '';
+}
+
+// init
+function init() {
+  // Add Todo
+  form.addEventListener('submit', addTodo);
+}
+
+init();
